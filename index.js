3fac1b64f6d1005ad4715c645c376dbc465981cb
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-06 02:57:41 +0600

    delete operation for booking completed

--- index.js
+++ index.js
@@ -68,6 +68,12 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         await client.db("admin").command({ ping: 1 });
